1937536a385205d7c8e5a6f7b37e7975b53503f3
TechWorldG <gt2468@outlook.com>
2020-01-31 11:15:28 +0000

    Add files via upload

---
+++ script-index.js
@@ -0,0 +1,198 @@
+(function($) { // Begin jQuery
+    $(function() { // DOM ready
+      // If a link has a dropdown, add sub menu toggle.
+      $('nav ul li a:not(:only-child)').click(function(e) {
+        $(this).siblings('.nav-dropdown').toggle();
+        // Close one dropdown when selecting another
+        $('.nav-dropdown').not($(this).siblings()).hide();
+        e.stopPropagation();
+      });
+      // Clicking away from dropdown will remove the dropdown class
+      $('html').click(function() {
+        $('.nav-dropdown').hide();
+      });
+      // Toggle open and close nav styles on click
+      $('#nav-toggle').click(function() {
+        $('nav ul').slideToggle();
+      });
+      // Hamburger to X toggle
+      $('#nav-toggle').on('click', function() {
+        this.classList.toggle('active');
+      });
+    }); // end DOM ready
+  })(jQuery); // end jQuery
+
+
+
+
+
+//Panels of images
+
+
+
+
+(function($) {
+  var speed = 2000;
+  var container =  $('.display-animation');
+  container.each(function() {
+      var elements = $(this).children();
+      elements.each(function() {
+          var elementOffset = $(this).offset();
+          var offset = elementOffset.left*0.8 + elementOffset.top;
+          var delay = parseFloat(offset/speed).toFixed(2);
+          $(this)
+              .css("-webkit-animation-delay", delay+'s')
+              .css("-o-animation-delay", delay+'s')
+              .css("animation-delay", delay+'s')
+              .addClass('animated');
+      });
+  });
+})(jQuery);
+
+/**
+* Created by Kupletsky Sergey on 04.09.14.
+*
+* Ripple-effect animation
+* Tested and working in: ?IE9+, Chrome (Mobile + Desktop), ?Safari, ?Opera, ?Firefox.
+* JQuery plugin add .ink span in element with class .ripple-effect
+* Animation work on CSS3 by add/remove class .animate to .ink span
+*/
+
+(function($) {
+  $(".ripple-effect").click(function(e){
+      var rippler = $(this);
+
+      // create .ink element if it doesn't exist
+      if(rippler.find(".ink").length == 0) {
+          rippler.append("<span class='ink'></span>");
+      }
+
+      var ink = rippler.find(".ink");
+
+      // prevent quick double clicks
+      ink.removeClass("animate");
+
+      // set .ink diametr
+      if(!ink.height() && !ink.width())
+      {
+          var d = Math.max(rippler.outerWidth(), rippler.outerHeight());
+          ink.css({height: d, width: d});
+      }
+
+      // get click coordinates
+      var x = e.pageX - rippler.offset().left - ink.width()/2;
+      var y = e.pageY - rippler.offset().top - ink.height()/2;
+
+      // set .ink position and add class .animate
+      ink.css({
+          top: y+'px',
+          left:x+'px'
+      }).addClass("animate");
+  })
+})(jQuery);
+
+
+
+
+//Video play/pause :)
+
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function pause() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ------------- VARIABLES ------------- //
+var ticking = false;
+var isFirefox = (/Firefox/i.test(navigator.userAgent));
+var isIe = (/MSIE/i.test(navigator.userAgent)) || (/Trident.*rv\:11\./i.test(navigator.userAgent));
+var scrollSensitivitySetting = 30; //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive) 
+var slideDurationSetting = 400; //Amount of time for which slide is "locked"
+var currentSlideNumber = 0;
+var totalSlideNumber = $(".background").length;
+
+// ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
+function parallaxScroll(evt) {
+  if (isFirefox) {
+    //Set delta for Firefox
+    delta = evt.detail * (-120);
+  } else if (isIe) {
+    //Set delta for IE
+    delta = -evt.deltaY;
+  } else {
+    //Set delta for all other browsers
+    delta = evt.wheelDelta;
+  }
+
+  if (ticking != true) {
+    if (delta <= -scrollSensitivitySetting) {
+      //Down scroll
+      ticking = true;
+      if (currentSlideNumber !== totalSlideNumber - 1) {
+        currentSlideNumber++;
+        nextItem();
+      }
+      slideDurationTimeout(slideDurationSetting);
+    }
+    if (delta >= scrollSensitivitySetting) {
+      //Up scroll
+      ticking = true;
+      if (currentSlideNumber !== 0) {
+        currentSlideNumber--;
+      }
+      previousItem();
+      slideDurationTimeout(slideDurationSetting);
+    }
+  }
+}
+
+// ------------- SET TIMEOUT TO TEMPORARILY "LOCK" SLIDES ------------- //
+function slideDurationTimeout(slideDuration) {
+  setTimeout(function() {
+    ticking = false;
+  }, slideDuration);
+}
+
+// ------------- ADD EVENT LISTENER ------------- //
+var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
+window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
+
+// ------------- SLIDE MOTION ------------- //
+function nextItem() {
+  var $previousSlide = $(".background").eq(currentSlideNumber - 1);
+  $previousSlide.removeClass("up-scroll").addClass("down-scroll");
+}
+
+function previousItem() {
+  var $currentSlide = $(".background").eq(currentSlideNumber);
+  $currentSlide.removeClass("down-scroll").addClass("up-scroll");
+}
